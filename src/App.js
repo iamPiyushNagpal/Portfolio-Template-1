4f1a058a263f5c531f974b565aa80b19dfc4ea87
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-06-27 11:19:50 +0530

    Add Intro Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Intro from './components/Intro/Intro';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="sections">
+        <Intro />
+      </div>
     </div>
   );
 }
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,19 @@
+import './Intro.css';
+import IntroImage from '../../assets/images/IntroImage.svg';
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <div className="intro__text">
+                <h3>Hi There</h3>
+                <h1>I'm John Doe</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div className="intro__image">
+                <img src={IntroImage} alt="" />
+            </div>
+        </div>
+    );
+}
+
+export default Intro;
