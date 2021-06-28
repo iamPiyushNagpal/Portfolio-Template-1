26c135536561b8a29e0c276a68fb9397db15fb66
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-06-28 20:38:54 +0530

    Open links in new tab

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,11 +10,11 @@ const Contact = () => {
             <h1>Contact</h1>
             <div className="contact">
                 <div className="contact__options">
-                    <a href="#"><FontAwesomeIcon className="mail" icon={faEnvelope} /></a>
-                    <a href=""><FontAwesomeIcon className="facebook" icon={faFacebook} /></a>
-                    <a href=""><FontAwesomeIcon className="linkedin" icon={faLinkedin} /></a>
-                    <a href=""><FontAwesomeIcon className="telegram" icon={faTelegram} /></a>
-                    <a href=""><FontAwesomeIcon className="github" icon={faGithub} /></a>
+                    <a href="#" target="_blank"><FontAwesomeIcon className="mail" icon={faEnvelope} /></a>
+                    <a href="" target="_blank"><FontAwesomeIcon className="facebook" icon={faFacebook} /></a>
+                    <a href="" target="_blank"><FontAwesomeIcon className="linkedin" icon={faLinkedin} /></a>
+                    <a href="" target="_blank"><FontAwesomeIcon className="telegram" icon={faTelegram} /></a>
+                    <a href="" target="_blank"><FontAwesomeIcon className="github" icon={faGithub} /></a>
                 </div>
                 <div className="contact__image">
                     <img src={ContactImage} alt="" />
